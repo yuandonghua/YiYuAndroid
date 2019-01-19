7fcbd485751a6be3e7c6c5e7f55af8873a1a0d67
yuandonghua <yuandh2@lenovo.com>
2019-01-19 15:00:55 +0800

    新增主题切换动画

--- app/src/main/java/com/yixinhuayuan/yiyu/mvp/ui/fragment/MyFragment.java
+++ app/src/main/java/com/yixinhuayuan/yiyu/mvp/ui/fragment/MyFragment.java
@@ -1,6 +1,8 @@
 package com.yixinhuayuan.yiyu.mvp.ui.fragment;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Message;
 import android.support.annotation.NonNull;
@@ -9,11 +11,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.jess.arms.base.BaseFragment;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
 
+import com.wuyr.rippleanimation.RippleAnimation;
 import com.yixinhuayuan.yiyu.di.component.DaggerMyComponent;
 import com.yixinhuayuan.yiyu.mvp.contract.MyContract;
 import com.yixinhuayuan.yiyu.mvp.presenter.MyPresenter;
@@ -25,6 +31,7 @@ import com.yixinhuayuan.yiyu.mvp.ui.view.ToggleSettings;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import butterknife.BindView;
+import timber.log.Timber;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
@@ -42,8 +49,22 @@ import static com.jess.arms.utils.Preconditions.checkNotNull;
  * ================================================
  */
 public class MyFragment extends BaseFragment<MyPresenter> implements MyContract.View {
+    /**
+     * 主题切换按钮
+     */
     @BindView(R.id.dayNightToggleButton)
     DayNightToggleButton dayNightToggleButton;
+    /**
+     * 根布局
+     */
+    @BindView(R.id.container)
+    ViewGroup container;
+    /**
+     * 子view数组
+     */
+    private View[] mChildViews;
+    private boolean mDayOrNight = true;
+    private int mDayOrNightColor;
 
     public static MyFragment newInstance() {
         MyFragment fragment = new MyFragment();
@@ -67,7 +88,7 @@ public class MyFragment extends BaseFragment<MyPresenter> implements MyContract.
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
-
+//        initStatusBar();
         initThemeButton();
         //跳转登陆页面
 //        NavHostFragment.findNavController(this).navigate(R.id.action_myFragment_to_loginActivity);
@@ -77,14 +98,98 @@ public class MyFragment extends BaseFragment<MyPresenter> implements MyContract.
      * 初始化主题风格view
      */
     private void initThemeButton() {
+        //获取所有子view
+        mChildViews = new View[container.getChildCount()];
+        for (int i = 0; i < mChildViews.length; i++) {
+            mChildViews[i] = container.getChildAt(i);
+        }
+
         ToggleSettings mBuilderSettings = new ToggleSettings.Builder()
                 .setToggleUnCheckedColor(getResources().getColor(R.color.colorPrimaryDark))
                 .setBackgroundUncheckedColor(getResources().getColor(R.color.gray))
-                .setToggleCheckedColor(getResources().getColor(R.color.colorAccent))
-                .setBackgroundCheckedColor(getResources().getColor(R.color.colorPrimaryDark))
+                .setToggleCheckedColor(getResources().getColor(R.color.white))
+                .setBackgroundCheckedColor(getResources().getColor(R.color.gray))
                 .setDuration(200)
                 .buildSettings();
         dayNightToggleButton.setToggleSettings(mBuilderSettings);
+        dayNightToggleButton.setOnCheckChangeListener(new DayNightToggleButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(View buttonView, boolean isChecked) {
+                //夜间模式切换
+                //切换动画
+                RippleAnimation.create(buttonView).setDuration(250).start();
+                //日间操作
+                if (mDayOrNight) {
+                    mDayOrNight = false;
+                    mDayOrNightColor = getResources().getColor(R.color.black);
+                } else {
+                    //夜间操作
+                    mDayOrNight = true;
+                    mDayOrNightColor = getResources().getColor(R.color.white);
+                }
+
+
+//                switch (view.getId()) {
+//                    case R.id.red:
+//                        color = Color.RED;
+//                        break;
+//                    case R.id.green:
+//                        color = Color.GREEN;
+//                        break;
+//                    case R.id.blue:
+//                        color = Color.BLUE;
+//                        break;
+//                    case R.id.yellow:
+//                        color = Color.YELLOW;
+//                        break;
+//                    case R.id.black:
+//                        color = Color.DKGRAY;
+//                        break;
+//                    case R.id.cyan:
+//                        color = Color.CYAN;
+//                        break;
+//                    default:
+//                        color = Color.TRANSPARENT;
+//                        break;
+//                }
+                updateColor(mDayOrNightColor);
+
+            }
+        });
+    }
+
+    /**
+     * 更新UI主题颜色
+     *
+     * @param color
+     */
+    private void updateColor(int color) {
+        for (View view : mChildViews) {
+            if (view instanceof TextView) {
+                ((TextView) view).setTextColor(color);
+            } else {
+                view.setBackgroundColor(color);
+            }
+        }
+
+    }
+
+    private void initStatusBar() {
+        Window window = getActivity().getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                        | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(Color.TRANSPARENT);
+                window.setNavigationBarColor(Color.TRANSPARENT);
+            }
+        }
     }
 
     /**
